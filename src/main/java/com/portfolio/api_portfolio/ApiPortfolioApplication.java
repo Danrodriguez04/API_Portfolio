624bf2421589d568e10b12e542750ec76f0aa889
Daniel <danielrodriguez@carnicassunyer.com>
2025-03-21 17:21:47 +0100

    init portfolio project

---
+++ src/main/java/com/portfolio/api_portfolio/ApiPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.api_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPortfolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portfolio/api_portfolio/ApiPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.api_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
